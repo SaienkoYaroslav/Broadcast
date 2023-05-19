fde180a811ad536ef618d4fdf41c468292860f89
SaienkoYaroslav <saienko.yaroslav01@gmail.com>
2023-05-19 13:48:56 +0300

    multi actions

--- app/src/main/java/com/example/broadcast/MyReceiver.kt
+++ app/src/main/java/com/example/broadcast/MyReceiver.kt
@@ -12,6 +12,11 @@ class MyReceiver: BroadcastReceiver() {
             Intent.ACTION_BATTERY_LOW -> {
                 Toast.makeText(p0, "Battery low", Toast.LENGTH_SHORT).show()
             }
+            Intent.ACTION_AIRPLANE_MODE_CHANGED -> {
+                val turnedOn = p1.getBooleanExtra("state", false)
+                Toast.makeText(p0, "Airplane mode is changed. Turned on: $turnedOn",
+                    Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
